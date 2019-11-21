57d2269db31cd74bb362741d9ca15dc98daba70a
nmorriss7109 <nmorriss7109@gmail.com>
2019-11-21 11:32:34 -0800

    File path now works on both DOS and Unix/GNU.

--- scraper/resize.py
+++ scraper/resize.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from PIL import Image
 import os, sys
 
 #Resize images
-directory = r"C:\Users\Nathan\Desktop\COGS 300\TrashClass\scraper\downloads"
+directory = Path("downloads/")
+directory = str(directory.resolve())
+print(directory)
 subDirs = [x[0] for x in os.walk(directory)]
 for path in subDirs:
     print(path)
@@ -11,13 +14,14 @@ for path in subDirs:
     def resize():
         count = 0
         for item in dirs:
-            if os.path.isfile(path+"\\"+item):
+            itemPath = os.path.join(path, item)
+            if os.path.isfile(itemPath):
                 count+=1
-                im = Image.open(path+"\\"+item)
-                f, e = os.path.splitext(path+"\\"+item)
+                im = Image.open(itemPath)
+                f, e = os.path.splitext(itemPath)
                 imResize = im.resize((512,384), Image.ANTIALIAS)
-                os.remove(path+"\\"+item)
-                imResize.save(path + "\\" + os.path.basename(path) + str(count) + '.jpg', 'JPEG', quality=90)
+                os.remove(itemPath)
+                imResize.save(os.path.join(path, os.path.basename(path) + str(count) + '.jpg'), 'JPEG', quality=90)
         return count
 
     print("Resized " + str(resize()) + " images.")
